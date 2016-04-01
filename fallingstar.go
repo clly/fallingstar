5cfea091c09280e55921d14c43c80683a2137949
Connor Kelly <connor.r.kelly@gmail.com>
2016-03-31 23:08:32 -0500

    Allow username as arg and update repos
    Change fallingstar so that users can now set the username as an argument. In addition if the path already exists attempt to pull instead. Doesn't make sure the directory is a git repo before attempting to pull.

--- fallingstar.go
+++ fallingstar.go
@@ -11,24 +11,44 @@ import (
 )
 
 func main() {
+	var user string
+	if (len(os.Args) >= 2) {
+		user = os.Args[1]
+	}
 	client := github.NewClient(nil)
-	starredRepos, resp, err := client.Activity.ListStarred("clly", nil)
+	starredRepos, resp, err := client.Activity.ListStarred(user, nil)
 	if err != nil {
-		panic(err)
+		fmt.Errorf("Failed to pull starred repos: %s", err)
 	}
 	for i := range(starredRepos) {
 		repo := starredRepos[i].Repository
 		fullName := *repo.FullName
-		fmt.Printf("Going to clone into %s via %s\n", fullName, *repo.CloneURL)
 		_, err := os.Stat(fullName)
 		if err != nil {
-			clone(*repo.FullName, *repo.CloneURL)
+			fmt.Printf("Going to clone into %s via %s\n", fullName, *repo.CloneURL)
+			clone(fullName, *repo.CloneURL)
 			time.Sleep(time.Second * 5)
 		} else {
 			fmt.Printf("Unable to clone into path %s. It already exists\n", fullName)
+			pull(fullName)
+			time.Sleep(time.Second * 1)
 		}
 	}
-	fmt.Println(resp)
+	fmt.Printf("Github api limit: %v. Github api remaining: %v. Github api reset %v\n", resp.Limit, resp.Remaining, resp.Reset)
+}
+
+func pull(path string) {
+	command := exec.Command("git", "pull", "--ff-only")
+	command.Dir = path
+	// TODO: Replicating from git clone. Combine functions
+	var out bytes.Buffer
+	command.Stdout = &out
+	command.Stderr = &out
+	err := command.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Pulling updates from upstream\n%s\n", out.String())
 }
 
 func clone(path string, url string) {
